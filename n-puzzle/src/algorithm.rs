ed9c2967dbc070686333bacb9665fc3ee6ff04d7
Tulipe <romain.dupeux24@gmail.com>
2025-04-15 19:09:19 +0200

    c

--- n-puzzle/src/algorithm.rs
+++ n-puzzle/src/algorithm.rs
@@ -1,4 +1,21 @@
-use crate::Tree::{Node, Tree};
-use crate::arena::Puzzle;
+pub mod heuristics {
+    use crate::Tree::{Node, Tree};
+    use crate::arena::Puzzle;
+    type PContainer = Vec<Vec<u16>>;
 
-pub mod Heuristics {}
+    fn hamming_distance(p: Puzzle, reference: PContainer) -> i32 {
+        let mut counter = 0;
+
+        for i in 0..p.dim {
+            for j in 0..p.dim {
+                if p.puzzle[i][j] == 0 {
+                    continue;
+                } else if p.puzzle[i][j] != reference[i][j] {
+                    counter += 1;
+                }
+            }
+        }
+
+        counter
+    }
+}
