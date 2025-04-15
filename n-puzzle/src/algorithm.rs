f7e0abdee9fd0a1a2e8078838a1a86b3205068ae
TulipeSoyeuse <romain.dupeux24@gmail.com>
2025-04-15 19:09:05 +0200

    b

--- n-puzzle/src/algorithm.rs
+++ n-puzzle/src/algorithm.rs
@@ -1,4 +1,10 @@
 use crate::Tree::{Node, Tree};
 use crate::arena::Puzzle;
 
-pub mod Heuristics {}
+pub mod heuristics {
+    enum Heuristic {
+        AStarAlgorithm(&mut Node),
+        IDA_Algorithm(&mut Node)
+    }
+    set_heuristic();
+}
